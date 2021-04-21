70418dd476a34d18165202eea6a8a99d0ac2a208
Thilo Maier <git@maier.asia>
2021-04-20 21:56:38 -0400

    Add initial homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,62 @@
+import {
+  ChartBarIcon,
+  LightningBoltIcon,
+  LightBulbIcon,
+} from '@heroicons/react/solid';
+import Layout from '../components/layout';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const areas = [
+  {
+    name: 'Data visualization',
+    description:
+      'I learn data visualization with React libraries that are built with D3. ',
+    icon: ChartBarIcon,
+  },
+  {
+    name: 'Next.js',
+    description:
+      'I learn how to build super fast websites with Next.js, the leading React framework.',
+    icon: LightningBoltIcon,
+  },
+  {
+    name: 'Developer productivity',
+    description:
+      'I keep improving my developer workflows to be more productive in my everyday work.',
+    icon: LightBulbIcon,
+  },
+];
+
+export default function Homepage() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Thilo Maier – Learn with me.</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+      <div className="text-center py-12 lg:py-20">
+        <p className="font-extrabold text-4xl sm:text-5xl lg:text-6xl sm:tracking-tight">
+          Learn with me.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <p className="max-w-xl mt-5 mx-auto text-xl text-text-light">
+          Hi, my name is Thilo. I am a developer based in Queens, NY. I learn
+          new things and share what I have learned.
+        </p>
+      </div>
+      <dl className="space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8 mb-8">
+        {areas.map((area) => (
+          <div key={area.name}>
+            <dt>
+              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-primary-default text-background-default mb-3">
+                <area.icon className="h-6 w-6" aria-hidden="true" />
+              </div>
+              <p className="text-2xl leading-relaxed font-medium">
+                {area.name}
+              </p>
+            </dt>
+            <dd className="ext-base text-text-light">{area.description}</dd>
+          </div>
+        ))}
+      </dl>
+    </Layout>
   );
 }
