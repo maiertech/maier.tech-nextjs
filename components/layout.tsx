41b84cad60557cc273504d2fe1f10e1be9324a5c
Thilo Maier <git@maier.asia>
2021-05-20 13:45:44 -0400

    Add header Fixes #29

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,22 @@
-import Container from '../components/container';
+import Header from '@/components//header';
+import Footer from '@/components/footer';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return <Container>{children}</Container>;
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-shrink-0 mb-6">
+        <Header />
+      </div>
+      <div className="flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-6">
+        {children}
+      </div>
+      <div className="flex-shrink-0">
+        <Footer />
+      </div>
+    </div>
+  );
 }
