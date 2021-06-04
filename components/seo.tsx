b3555b196ba5ebd9c165ff7693e90cd0d9187a44
Thilo Maier <git@maier.asia>
2021-06-04 01:03:51 -0400

    Use next-seo for SEO Fixes #65

--- components/seo.tsx
+++ components/seo.tsx
@@ -1,43 +1,23 @@
-import Head from 'next/head';
 import { useRouter } from 'next/router';
-
-function decorate(title: string, path: string) {
-  if (path === '/') return title;
-  return `${title} – Thilo Maier`;
-}
+import { NextSeo } from 'next-seo';
+import pkg from '../package.json';
 
 type Props = {
-  siteUrl?: string;
-  title: string;
+  title?: string;
   description: string;
-  keywords?: string[];
   canonicalUrl?: string;
 };
 
-export default function SEO({
-  title,
-  description,
-  keywords,
-  siteUrl = 'https://maier.tech',
-  canonicalUrl,
-}: Props) {
+export default function SEO({ title, description, canonicalUrl }: Props) {
   const router = useRouter();
   const path = router.asPath;
-  const url = canonicalUrl ? canonicalUrl : `${siteUrl}${path}`;
-  const decoratedTitle = decorate(title, path);
+  const url = `${pkg.homepage}${path}`;
   return (
-    <Head>
-      <title>{decoratedTitle}</title>
-      <meta name="description" content={description} />
-      {keywords && <meta name="keywords" content={keywords.join(', ')} />}
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={decoratedTitle} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:url" content={url} />
-      <meta name="twitter:title" content={decoratedTitle} />
-      <meta name="twitter:description" content={description} />
-      <link rel="canonical" href={url} />
-    </Head>
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={canonicalUrl ?? url}
+      openGraph={{ url, title, description }}
+    />
   );
 }
